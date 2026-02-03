94f2f72d30ce3e4982a1f7d99eaa7ceca348d9e1
Victoria Steingaszner <nm24770@bristol.ac.uk>
2026-02-03 18:10:18 +0000

    added documentation

--- R/functions.r
+++ R/functions.r
@@ -1,4 +1,11 @@
 
+#' Extract files from the .tar archive and move them to the results directory
+#' @param tar.file .tar archive containing files of interest 
+#' @param extract.file files from the .tar archive to extract
+#' @param new.file file to move extracted data files into
+#' @param resultsdir where the new.file will be saved
+#' @return untarred data file moved to resultsdir/new.file
+#' @export
 extract.file <- function(tar.file, extract.file, new.file, resultsdir) {
   # get file path to extracted file
   x.file <-
@@ -19,15 +26,28 @@ extract.file <- function(tar.file, extract.file, new.file, resultsdir) {
   unlink(dirname(x.file), recursive = TRUE)
 }
 
+#' Create a table and save it to a directory
+#' @param x dataframe to save 
+#' @param filename path and filename to save the dataframe to
+#' @return a saved dataframe
+#' @export
 my.write.table <- function(x, filename) {
   cat("saving", basename(filename), "...\n")
   write.table(x, file = filename, row.names = T, col.names = T, sep = "\t")
 }
 
+#' Extract participant ID from TCGA barcode
+#' @param id TCGA barcode
+#' @return participant ID
+#' @export
 extract.participant <- function(id) {
   sub("TCGA-[^-]+-([^-]+)-.*", "\\1", id)
 }
 
+#' Extract tissue type from TCGA barcode
+#' @param id TCGA barcode
+#' @return Tissue type
+#' @export
 extract.tissue <- function(id) {
   sub("TCGA-[^-]+-[^-]+-([0-9]+)[^-]+-.*", "\\1", id)
 }
